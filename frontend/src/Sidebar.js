680e429d2015f9596311f9bb6d269189d21b99b6
Kevin Salcedo <kevsalcedo@gmail.com>
2018-09-11 21:00:59 -0500

    sidebar now dynamically loads 5 latest blog entries

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -3,9 +3,26 @@ import { Col, InputGroup, Input,
   Navbar, Nav, NavItem, NavLink,
   NavbarBrand, Button } from 'reactstrap';
 
+
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Sidebar extends React.Component {
+  state = {
+    recent_posts: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const recent_posts = await res.json();
+      this.setState({
+        recent_posts
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <Col xs='3'>
@@ -13,27 +30,17 @@ class Sidebar extends React.Component {
           <br/>
           <br/>
           <br/>
-          <br/>{/* Get rid of this shit */} 
+          <br/>{/* Get rid of this shit */}
           <InputGroup>
             <Input placeholder="Search" />
           </InputGroup>
           <NavbarBrand href='/'>Recent Blog Posts</NavbarBrand>
           <Nav vertical justified>
-            <NavItem>
-              <NavLink href='#'>Post 1</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='#'>Post 2</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='#'>Post 3</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='#'>Post 4</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='#'>Post 5</NavLink>
-            </NavItem>
+            {this.state.recent_posts.map(post => (
+              <NavItem key={post.id}>
+                <NavLink href={`/posts/${post.id}`}>{post.title}</NavLink>
+              </NavItem>
+            ))}
             <hr/>
             <NavItem>
               <Button color="primary">Archive</Button>
