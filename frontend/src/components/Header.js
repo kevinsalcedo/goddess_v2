78d64403c114ceb9b6e99c4357e96833c5e71d2d
Kevin Salcedo <kevsalcedo@gmail.com>
2018-09-25 18:32:53 -0500

    added two typefaces - poly for body text and agane for headings

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
+  NavLink
 } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/Header.css';
@@ -26,28 +26,37 @@ class Header extends React.Component {
     });
   }
   render() {
-    return (<div>
-      <Navbar className="navi" fluid={'top'} light="light" expand='md'>
-        <NavbarBrand href='/' ><span className="header-item">Goddess Climbing</span></NavbarBrand>
-        <NavbarToggler onClick={this.toggle}/>
-        <Collapse isOpen={this.state.isOpen} navbar="navbar">
-          <Nav className='ml-auto align-items-end' navbar="navbar">
-            <NavItem>
-              <NavLink href='/'><span className="header-item">Home</span></NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='/about'><span className="header-item">About</span></NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='/blog'><span className="header-item">Blog</span></NavLink>
-            </NavItem>
-            <NavLink href='/contact'>
-              <span className="header-item">Contact</span>
+    return (
+  <div className="header-img">
+    <Navbar navbar-fixed-top light expand='md'>
+      <NavbarBrand href='/'>
+        <span className="header-item">Goddess Climbing</span>
+      </NavbarBrand>
+      <NavbarToggler onClick={this.toggle}/>
+      <Collapse isOpen={this.state.isOpen} navbar>
+        <Nav className='ml-auto' navbar>
+          <NavItem>
+            <NavLink href='/'>
+              <span className="header-item">Home</span>
             </NavLink>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>);
+          </NavItem>
+          <NavItem>
+            <NavLink href='/about'>
+              <span className="header-item">About</span>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href='/blog'>
+              <span className="header-item">Blog</span>
+            </NavLink>
+          </NavItem>
+          <NavLink href='/contact'>
+            <span className="header-item">Contact</span>
+          </NavLink>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  </div>);
   }
 }
 
