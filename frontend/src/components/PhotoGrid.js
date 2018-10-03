36a43c877f2799106b9ec337717c2d1601767525
Kevin Salcedo <kevsalcedo@gmail.com>
2018-10-03 09:31:15 -0500

    enabled photo upload

--- frontend/src/components/PhotoGrid.js
+++ frontend/src/components/PhotoGrid.js
@@ -18,7 +18,8 @@ import {
   Form,
   FormGroup,
   Label,
-  Input
+  Input,
+  FormText
 } from 'reactstrap';
 import Error404 from './error404';
 import ImgsViewer from 'react-images-viewer';
@@ -33,15 +34,22 @@ class PhotoGrid extends React.Component {
     super(props);
     this.state = {
       all_photos: [],
-      currPhoto: {src: "", caption: ""},
+      currPhoto: {
+        src: "",
+        caption: ""
+      },
       isOpen: false,
-      modalOpen: false
+      modalOpen: false,
+      upload: {}
     };
     this.handleViewer = this.handleViewer.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async componentDidMount() {
     this.updateData();
+    this.setState({upload: new FormData()})
   }
 
   updateData = () => {
@@ -67,6 +75,42 @@ class PhotoGrid extends React.Component {
     });
   }
 
+  handleChange(event, inputType) {
+    const upload = this.state.upload;
+    if (inputType === "file") {
+      upload.append('src', event.target.files[0]);
+    }
+    if (inputType === "location") {
+      upload.append('location', event.target.value);
+    }
+    if (inputType === "climber") {
+      upload.append('climber', event.target.value);
+    }
+    if (inputType === "author") {
+      upload.append('aytgir', event.target.value);
+    }
+    if (inputType === "caption") {
+      upload.append('caption', event.target.value);
+    }
+    this.setState({upload});
+  }
+
+  handleSubmit() {
+    const upload = this.state.upload;
+    let data = new FormData();
+    data.append('climber', upload.climber);
+    data.append('location', upload.location);
+    data.append('author', upload.author);
+    data.append('caption', upload.caption);
+
+    fetch('http://127.0.0.1:8000/api/upload/', {
+      method: 'POST',
+      body: upload
+    })
+
+    this.setState({modalOpen: false})
+  }
+
   render() {
     const {all_photos, isOpen, currPhoto} = this.state;
 
@@ -77,7 +121,9 @@ class PhotoGrid extends React.Component {
             <h1>User Submitted Photos</h1>
           </Col>
           <Col xs="12" md="3">
-            <Button onClick={() => {this.setState({modalOpen: true})}}>Upload</Button>
+            <Button onClick={() => {
+                this.setState({modalOpen: true})
+              }}>Upload</Button>
           </Col>
         </Row>
         <CardColumns className="photo_grid">
@@ -109,31 +155,60 @@ class PhotoGrid extends React.Component {
             Submit Your Own Photo
           </ModalHeader>
           <ModalBody>
-            <Form>
-              <FormGroup>
-                <Label for="SRC">Image</Label>
-                <Input type="file" name="src" id="SRC"/>
+            <Form method="post" action="http://127.0.0.1:8000/api/upload/">
+              <FormGroup row="row">
+                <Label for="SRC" sm={2}>Image</Label>
+                <Col sm={10}>
+                  <Input onChange={(e) => {
+                      this.handleChange(e, "file");
+                    }} type="file" name="src" id="SRC"/>
+                  <FormText></FormText>
+                </Col>
               </FormGroup>
-              <FormGroup>
-                <Label for="CLIMBER">Climber</Label>
-                <Input type="text" name="climber" id="CLIMBER"/>
+              <FormGroup row="row">
+                <Label for="CLIMBER" sm={2}>Climber</Label>
+                <Col sm={10}>
+                  <Input onChange={(e) => {
+                      this.handleChange(e, "climber");
+                    }} type="text" name="climber" id="CLIMBER"/>
+                </Col>
               </FormGroup>
-              <FormGroup>
-                <Label for="LOCATION">Location</Label>
-                <Input type="text" name="location" id="LOCATION"/>
+              <FormGroup row="row">
+                <Label for="LOCATION" sm={2}>Location</Label>
+                <Col sm={10}>
+                  <Input onChange={(e) => {
+                      this.handleChange(e, "location");
+                    }} type="text" name="location" id="LOCATION"/>
+                </Col>
               </FormGroup>
-              <FormGroup>
-                <Label for="CAPTION">Climber</Label>
-                <Input type="textarea" name="caption" id="CAPTION"/>
+              <FormGroup row="row">
+                <Label for="AUTHOR" sm={2}>author</Label>
+                <Col sm={10}>
+                  <Input onChange={(e) => {
+                      this.handleChange(e, "author");
+                    }} type="text" name="author" id="AUTHOR"/>
+                </Col>
+              </FormGroup>
+              <FormGroup row="row">
+                <Label for="CAPTION" sm={2}>Caption</Label>
+                <Col>
+                  <Input onChange={(e) => {
+                      this.handleChange(e, "caption");
+                    }} type="textarea" name="caption" id="CAPTION"/>
+                </Col>
               </FormGroup>
             </Form>
           </ModalBody>
           <ModalFooter>
             <Col>
-              <Button>Submit</Button>
+              <Button onClick={() => {
+                  this.handleSubmit()
+                }}>Submit</Button>
             </Col>
             <Col>
-              <Button onClick={() => {this.setState({modalOpen: false})}}>Cancel</Button>
+              <Button onClick={() => {
+                  this.setState({modalOpen: false})
+                }}>Cancel</Button>
             </Col>
           </ModalFooter>
         </Modal>
