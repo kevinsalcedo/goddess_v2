18260009de2631e4ae3fc03beac63cf7519fa26d
Kevin Salcedo <kevsalcedo@gmail.com>
2019-01-19 12:59:16 -0600

    live endpoint and larger mobile paddingfor nav

--- frontend/src/components/global_endpoint.js
+++ frontend/src/components/global_endpoint.js
@@ -1,6 +1,6 @@
 let local = "http://127.0.0.1:8000";
 let live = "http://goddess-env.5k5d6mwb3p.us-east-1.elasticbeanstalk.com";
 
-let global_endpoint = local;
+let global_endpoint = live;
 
 export default global_endpoint;
